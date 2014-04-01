281b59e092de7441c3de54dfda6e4ff5fd479525
Cesare <cesarerocchi@gmail.com>
2014-04-01 15:29:03 +0200

    First commit

---
+++ DearDiary/SMPost.m
@@ -0,0 +1,13 @@
+//
+//  SMPost.m
+//  DearDiary
+//
+//  Created by Cesare Rocchi on 8/21/13.
+//  Copyright (c) 2013 Cesare Rocchi. All rights reserved.
+//
+
+#import "SMPost.h"
+
+@implementation SMPost
+
+@end
